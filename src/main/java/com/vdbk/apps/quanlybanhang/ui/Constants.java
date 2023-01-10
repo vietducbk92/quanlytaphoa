8454f3fc0e3ffaa951c80de82971a81c5af73be8
vietduc nguyen <vdbk92@gmail.com>
2023-01-10 14:28:00 +0700

    release version 1.0.4

--- src/main/java/com/vdbk/apps/quanlybanhang/ui/Constants.java
+++ src/main/java/com/vdbk/apps/quanlybanhang/ui/Constants.java
@@ -34,6 +34,6 @@ public class Constants {
     public static String STORE_PHONE_NUMBER = "Ms.Lý: 0377400834";
     public static String STORE_ADDRESS = "Châm khê-Phong Khê-TP.Bắc Ninh-BN";
     
-    public static String VERSION = "1.0.3";
+    public static String VERSION = "1.0.4";
     
 }
